1f6f7dcaac74e8758a80236447da538f1add14a5
nicobonder <nicobonder@hotmail.com>
2022-04-04 17:20:00 -0300

    small corrections in hover

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -128,7 +128,7 @@ const handleAuth = ()=>{
             </div>
             </Link>
           <Link to="/checkout-page">
-            <IconButton className='shoppingCart' sx={{ marginRight: 3 }}>
+            <IconButton className='shoppingCart' sx={{ marginRight: 6 }}>
             <Badge badgeContent={basket?.length} color="secondary" className='badgeCart' sx={{ margin: 0}}>
                     <ShoppingCart fontSize='large' className='cartButton' />
                 </Badge>
